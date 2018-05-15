3e66f91e5bc3b0092dcf816db5bae590b675d9f9
DESKTOP-8B650QJ\decof <deco.felipe11@gmail.com>
2018-05-15 18:35:25 -0300

    Troquei tudo

--- CopioServer/src/br/edu/unifei/copio/ConnectionProtocol.java
+++ CopioServer/src/br/edu/unifei/copio/ConnectionProtocol.java
@@ -3,12 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.unifei.copio;
+package br.edu.unifei.lab02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,7 +29,6 @@ public class ConnectionProtocol implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("olaola");
         int recvMsgSize;  // Size of receive message
         byte[] byteBuffer = new byte[1024];  // Receive buffer
         
@@ -33,12 +36,21 @@ public class ConnectionProtocol implements Runnable{
         s.getInetAddress().getHostAddress() + " on port " +
           s.getPort());
 
-        InputStream in;
         try {
-            in = s.getInputStream();
-            OutputStream out = s.getOutputStream();
-            while ((recvMsgSize = in.read(byteBuffer)) != -1)
-            System.out.write(byteBuffer, 0, recvMsgSize);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(this.s.getInputStream()));
+            PrintStream saida = new PrintStream(this.s.getOutputStream());
+            Servidor.nomeCliente = entrada.readLine();
+            
+            if (Servidor.Lista_Users(s, Servidor.nomeCliente)){
+                saida.println("Não foi possível conectar! Nome ja existente");
+                this.s.close();
+                return;
+            } else {
+                System.out.println(Servidor.nomeCliente + " conectou-se!");
+                System.out.println(Servidor.listaClientes.get(0).toString());
+                System.out.println(Servidor.listaClientes.get(1).toString());
+            }
+            
         } catch (IOException ex) {
             Logger.getLogger(ConnectionProtocol.class.getName()).log(Level.SEVERE, null, ex);
         }
