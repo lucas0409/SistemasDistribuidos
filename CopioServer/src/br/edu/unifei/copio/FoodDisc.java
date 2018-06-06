93c1f8c51496350574e5e6ed7a21e5ef371939de
Lucas Alves de Oliveira <lucasao0409@gmail.com>
2018-06-06 10:52:49 -0300

    s

--- CopioServer/src/br/edu/unifei/copio/FoodDisc.java
+++ CopioServer/src/br/edu/unifei/copio/FoodDisc.java
@@ -19,8 +19,8 @@ public class FoodDisc extends UnicastRemoteObject implements FoodDiscInterface {
     private final int margin = 10;
     private Point position;
     private int mass;
-    private final int screenWidth = 800;
-    private final int screenHeight = 800;
+    private final int screenWidth = 1280;
+    private final int screenHeight = 720;
             
     @Override
     public void eatThis() throws RemoteException {
@@ -40,6 +40,6 @@ public class FoodDisc extends UnicastRemoteObject implements FoodDiscInterface {
         position = new Point();
         position.x = (new Random()).nextInt(screenWidth - margin) + margin;
         position.y = (new Random()).nextInt(screenHeight - margin) + margin;
-        mass = (new Random()).nextInt(50) + 10;
+        mass = (new Random()).nextInt(5) + 10;
     }    
 }
