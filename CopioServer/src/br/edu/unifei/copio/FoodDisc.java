0015e0fe5fd2d6895ba3e58b95ec8041a60df12f
Lucas Alves de Oliveira <lucasao0409@gmail.com>
2018-07-01 15:41:50 -0300

    [IMP] melhoras

--- CopioServer/src/br/edu/unifei/copio/FoodDisc.java
+++ CopioServer/src/br/edu/unifei/copio/FoodDisc.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.unifei.copio;
 
 import java.awt.Point;
@@ -10,31 +5,32 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Random;
 
-/**
- *
- * @author lucas
- */
 public class FoodDisc extends UnicastRemoteObject implements FoodDiscInterface {
     private final int margin = 10;
-    private Point position;
-    private int mass;
-    private int screenWidth = 1000;
-    private int screenHeight = 600;
+    private Point position; //posição do disco
+    private int mass; //massa do disco
+    private int screenWidth = 800; //largura da janela do jogo
+    private int screenHeight = 600; //altura da janela do jogo
+    
+    //método para o cliente tentar comer o disco
     public synchronized int eatThis(Point p, double raio) throws RemoteException {
         if (position.distance(p) < raio){
-            position.x = (new Random()).nextInt(screenWidth) - margin;
-            position.y = (new Random()).nextInt(screenHeight) - margin;
+            position.x = (new Random()).nextInt(screenWidth - mass) + mass;
+            position.y = (new Random()).nextInt(screenHeight - mass) + mass;
         }else return -1;
         return mass;
     }
     @Override
     public Point getPosition() throws RemoteException { return position; }
+    
     @Override
     public int getMass() throws RemoteException { return mass; }
+    
+    //inicialização do disco
     public FoodDisc() throws RemoteException{
         position = new Point();
-        position.x = (new Random()).nextInt(screenWidth)-margin;   
-        position.y = (new Random()).nextInt(screenHeight)-margin;
+        position.x = (new Random()).nextInt(screenWidth - margin) + margin;   
+        position.y = (new Random()).nextInt(screenHeight - margin) + margin;
         mass = (new Random()).nextInt(5) + 2;
-    }    
+    }
 }
--- CopioServer/src/br/edu/unifei/copio/FoodDiscInterface.java
+++ CopioServer/src/br/edu/unifei/copio/FoodDiscInterface.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.unifei.copio;
 
 import java.awt.Point;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- *
- * @author lucas
- */
 public interface FoodDiscInterface extends Remote {
     public int eatThis(Point p, double raio) throws RemoteException;
     public Point getPosition() throws RemoteException;
